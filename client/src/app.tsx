5ff82ddd90671368759cfc315a9453db4ef2f360
airbus <ichbrauchemehrplatz@gmail.com>
2023-03-15 16:33:30 +0100

    configured query client on client

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,16 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { TestComp } from './test-comp';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            retry: false,
+            staleTime: Infinity,
+        },
+    },
+});
 
 export function App() {
     return (
--- client/src/test-comp.tsx
+++ client/src/test-comp.tsx
@@ -1,18 +1,24 @@
 import { useQuery } from 'react-query';
 import { config } from './config';
 
-export function TestComp() {
-    const { data, isLoading } = useQuery('get-two-random-cards', async () => {
-        const result = await fetch(`${config.serverUrl}/cards`);
+interface Card {
+    id: number;
+    image: string;
+}
 
-        return await result.json();
+interface Response {
+    first_card: Card;
+    second_card: Card;
+}
+
+export function TestComp() {
+    const { data } = useQuery<Response>('get-two-random-cards', async () => {
+        return await (await fetch(`${config.serverUrl}/cards`)).json();
     });
 
     return (
         <div>
-            {isLoading ? (
-                <div>Loading</div>
-            ) : (
+            {data && (
                 <div className="text-3xl flex font-bold bg-cyan-100 border-4 border-red-500">
                     <img src={data.first_card.image} />
                     <img src={data.second_card.image} />
