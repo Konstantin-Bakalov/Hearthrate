732d7c2008e6e76c9b4f09d8cc1186ec4edf14cc
airbus <ichbrauchemehrplatz@gmail.com>
2023-03-15 15:16:14 +0100

    created models for card and vote

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from datetime import datetime
+from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from config import SERVER_PORT, DB_SERVER
@@ -10,6 +11,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_SERVER
 db = SQLAlchemy(app)
 app.app_context().push()
+
+class Cards(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String, nullable=False)
+
+class Votes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voted_for_id = db.Column(db.Integer, db.ForeignKey('cards.id'))
+    voted_against_id = db.Column(db.Integer, db.ForeignKey('cards.id'))
+    date_voted = db.Column(db.Date, default=datetime.utcnow)
 
 @app.route("/")
 def hello_world():
